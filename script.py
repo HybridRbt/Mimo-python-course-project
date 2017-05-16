5de9ee6b08c7d941b24ecfef12e1971f879342e9
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-05-15 22:20:38 -0500

    add code for chap 1

--- script.py
+++ script.py
@@ -1,1 +1,9 @@
 # Mimo python course project
+# Chapter 1
+# This is my filmography project
+film1 = "12 Monkeys"
+film2 = "Brazil"
+film3 = "Zero Theorem"
+print(film1)
+print(film2)
+print(film3) 
