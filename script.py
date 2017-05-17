dc91a9b73c6661d518fa9bb009b4e5427280a7e7
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-05-16 22:37:09 -0500

    add code for chap 2 & 3

--- script.py
+++ script.py
@@ -6,4 +6,23 @@ film2 = "Brazil"
 film3 = "Zero Theorem"
 print(film1)
 print(film2)
-print(film3) 
+print(film3)
+
+# Chapter 2
+film1 = ("12 Monkeys", 8.1)
+film2 = ("Brazil", 8.0)
+film3 = ("Zero Theorem", 6.1)
+print(film1[0] + " has " + str(film1[1]) + " points")
+
+# Chapter 3
+film4 = ('''Monty Python\'s \
+Life of Brian''', 7.2)
+summary = film4[0] + \
+":\nBrian is mistaken for Jesus"
+print(summary)
+
+q = 'What\'s the air speed \
+velocity of an unladen swallow?'
+word = q[-8:-1]
+q = q.replace(word, "rabbit")
+print(q) 
