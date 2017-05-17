0836295cd6b1e03bd9509ea19c60719fb2468c40
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-05-16 22:39:01 -0500

    add boolean

--- script.py
+++ script.py
@@ -9,14 +9,14 @@ print(film2)
 print(film3)
 
 # Chapter 2
-film1 = ("12 Monkeys", 8.1)
-film2 = ("Brazil", 8.0)
-film3 = ("Zero Theorem", 6.1)
+film1 = ("12 Monkeys", 8.1, True)
+film2 = ("Brazil", 8.0, False)
+film3 = ("Zero Theorem", 6.1, True)
 print(film1[0] + " has " + str(film1[1]) + " points")
 
 # Chapter 3
 film4 = ('''Monty Python\'s \
-Life of Brian''', 7.2)
+Life of Brian''', 7.2, True)
 summary = film4[0] + \
 ":\nBrian is mistaken for Jesus"
 print(summary)
